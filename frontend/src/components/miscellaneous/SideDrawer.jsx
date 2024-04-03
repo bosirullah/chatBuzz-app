8c5e31b3c353410109a4594fde7447cc1356958f
bosirullah <mdbosirullah@gmail.com>
2024-04-03 22:46:50 +0530

    deploy not working part - 8

--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -28,8 +28,8 @@ import axios from "axios";
 import ChatLoading from "../../components/ChatLoading";
 import UserListItem from "../UserAvatar/UserListItem";
 import { getSender } from "../../config/ChatLogics";
-import NotificationBadge from "react-notification-badge";
-import { Effect } from "react-notification-badge";
+// import NotificationBadge from "react-notification-badge";
+// import { Effect } from "react-notification-badge";
 
 const SideDrawer = () => {
     const [search, setSearch] = useState("");
@@ -159,10 +159,10 @@ const SideDrawer = () => {
                 <div>
                     <Menu>
                         <MenuButton p={1}>
-                            <NotificationBadge
+                            {/* <NotificationBadge
                                 count={notification.length}
                                 effect={Effect.SCALE}
-                            />
+                            /> */}
                             <BellIcon fontSize="2xl" m={1} />
                         </MenuButton>
                         <MenuList pl={2}>
